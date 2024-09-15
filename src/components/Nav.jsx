170bd7022be692ef5ab1f7c31d950d8d44051ca9
chaudharyvicky453@gmail.com <user.email like : "chaudharyvicky564@gmail.com>
2024-09-15 13:16:07 +0530

    NavLink and style

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 
 
 function Nav() {
+  const styleLink = (e)=>{
+    return {
+        color: e.isActive ? "tomato" : ""
+    }
+  }
   return (
        <div>
          <nav className=' p-5 flex gap-10 justify-center font-semibold text-lg'>
-            <Link to="/">Home</Link>
-            <Link to="/user">User</Link>
-            <Link to="/about">About</Link>
+            <NavLink style={styleLink} to="/">Home</NavLink>
+            <NavLink style={styleLink} to="/user">User</NavLink>
+            <NavLink style={styleLink} to="/about">About</NavLink>
         </nav>
        </div>
   )
