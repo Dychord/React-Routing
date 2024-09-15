ad33c55597aa76b0f1323cdfcf3c661dcab78b67
chaudharyvicky453@gmail.com <chaudharyvicky5640@gmail.com>
2024-09-15 13:43:57 +0530

    className and .join in NavLink instead of style

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,16 +4,17 @@ import { Link, NavLink } from 'react-router-dom'
 
 function Nav() {
   const styleLink = (e)=>{
-    return {
-        color: e.isActive ? "tomato" : ""
-    }
+    return [
+        e.isActive ? "text-red-300" : "",
+        e.isActive ? "font-semibold" : "",
+    ].join(" ")
   }
   return (
        <div>
          <nav className=' p-5 flex gap-10 justify-center font-semibold text-lg'>
-            <NavLink style={styleLink} to="/">Home</NavLink>
-            <NavLink style={styleLink} to="/user">User</NavLink>
-            <NavLink style={styleLink} to="/about">About</NavLink>
+            <NavLink className={styleLink} to="/">Home</NavLink>
+            <NavLink className={styleLink} to="/user">User</NavLink>
+            <NavLink className={styleLink} to="/about">About</NavLink>
         </nav>
        </div>
   )
