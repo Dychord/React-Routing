924c6e252f356f0cd44e6776885b9f40a9502822
chaudharyvicky453@gmail.com <chaudharyvicky5640@gmail.com>
2024-09-15 14:36:14 +0530

    useNavigate

--- src/components/UserDetails.jsx
+++ src/components/UserDetails.jsx
@@ -1,15 +1,22 @@
 import React from 'react'
-import { Link, useParams } from 'react-router-dom'
+import { Link, useNavigate, useParams } from 'react-router-dom'
 
 
 export function UserDetails() {
   const {username} = useParams()
+  const navigate = useNavigate()
+  const goBackToUser = ()=>{
+    return navigate('/user')
+  }
+
+  
   return (
     <>
     <div className='flex flex-col items-center mt-10'>
       <h1 className='text-5xl font-bold text-red-400'>User Details</h1>
       <h3 className='mt-4 font-semibold text-2xl'>hello {username} </h3>
-      <Link to="/user"><button className='bg-red-400 rounded-md px-3 py1 mt-3 font-semibold'>Go Back</button></Link>
+      <button onClick={goBackToUser} className='bg-red-400 rounded-md px-3 py1 mt-3 font-semibold'>Go Back</button>
+      {/* <Link to="/user"><button className='bg-red-400 rounded-md px-3 py1 mt-3 font-semibold'>Go Back</button></Link> */}
     </div>
       
     </>
